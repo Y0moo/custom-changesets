660bf5b87aa11131695debc2952d24cb60235a10
Dmitry Kapeliushnyi <its.dmitrii.kpl@gmail.com>
2023-04-28 15:52:24 +0700

    xxx

---
+++ scripts/changesetRelease.sh
@@ -0,0 +1,4 @@
+# Convert changesets into a new version
+npx changeset version
+# Update lock-file (needed to avoid invalid lock-file with outdated versions)
+npm install --no-save --package-lock-only
